9dbea60eaa56c6dd955146e97bf59a38bf87ef6e
Alexandre Vallette <vallettea@gmail.com>
2015-09-16 09:31:36 +0200

    proper use of boot2docker

--- tests/mocha/broker.js
+++ tests/mocha/broker.js
@@ -10,76 +10,79 @@ var makeTcpReceiver = require('../../tools/makeTcpReceiver');
 var boot2dockerIp = require('../../tools/boot2dockerIp.js');
 var PRIVATE = require('../../PRIVATE.json');
 
-// boot2docker ip
-var host = '192.168.59.103';
+var host;
 
-// boot2dockerIp()
-// .then(function(host){
-// 	console.log('host', host);
+describe('Sensor initialization', function() {
 
-	describe('Sensor initialization', function() {
+	this.timeout(9000);
 
-		this.timeout(9000);
+	var sensorSocket;
+	var tcpSocketSensorReceiver;
 
-		var sensorSocket;
-		var tcpSocketSensorReceiver;
-
-		
-		// simulate sensor
-		beforeEach(function(ready){
-			sensorSocket = net.connect({
-		        host: host,
-		        port: 5100
-		    });
-
-			sensorSocket.on('connect', function(){
-		        tcpSocketSensorReceiver = makeTcpReceiver(sensorSocket, "\n");
-		        ready();
-		    });
-		});
-
-		it('broker should register unknown sensor if token ok', function (done) {
-			tcpSocketSensorReceiver.on('message', function(message) {
-
-	    		request.get({
-		            url: 'http://' + host + ':4000/allSensors'
-		        }, function(err, result, body){
-		            if (!err) {
-		            	var sensor = JSON.parse(body)[0];
-
-		            	expect(parseInt(sensor.sim)).to.equal(123456677999); 
-		                done();
-		            }
-		            else {
-		            	console.log('err', err);
-		            }
-		        });
-	    	});
-			sensorSocket.write("init 123456677999 " + PRIVATE.token + "\n");	
-		});
-
-		it('broker should send config parameters to sensor if token ok', function (done) {
-			tcpSocketSensorReceiver.on('message', function(message) {
-
-	    		console.log("received tcp data: ", message);
-	    		var args = message.slice(4);
-	    		var argsplit = args.split(" ");
-	    		expect(argsplit[0]).to.equal("init");
-	    		expect(isNaN(argsplit[1])).to.equal(false);
-	    		expect(isNaN(argsplit[2])).to.equal(false);
-	    		expect(isNaN(argsplit[3])).to.equal(false);
-	    		// check for proper datetime
-	    		expect(Date.parse(argsplit[4])).to.be.a("number");
-	    		done();
-	    	});
-			sensorSocket.write("init 123456677999 " + PRIVATE.token + "\n");
-		});
-
-		it('broker should not register sensor if token not ok', function () {
-			//TBD
-		});
+	// get host ip
+	before(function(ready){
+		boot2dockerIp()
+		.then(function(h){
+			host = h;
+			ready();
+		})
+		.catch(function(error){
+			console.log("Error determining the host");
+		});		
 	});
-// })
-// .catch(function(error){
-// 	console.log("Error determining the host");
-// });
+
+	// simulate sensor
+	beforeEach(function(ready){
+		sensorSocket = net.connect({
+	        host: host,
+	        port: 5100
+	    });
+
+		sensorSocket.on('connect', function(){
+	        tcpSocketSensorReceiver = makeTcpReceiver(sensorSocket, "\n");
+	        ready();
+	    });
+	});
+
+	it('broker should register unknown sensor if token ok', function (done) {
+		tcpSocketSensorReceiver.on('message', function(message) {
+
+    		request.get({
+	            url: 'http://' + host + ':4000/allSensors'
+	        }, function(err, result, body){
+	            if (!err) {
+	            	var sensor = JSON.parse(body)[0];
+
+	            	expect(parseInt(sensor.sim)).to.equal(123456677999); 
+	                done();
+	            }
+	            else {
+	            	console.log('err', err);
+	            }
+	        });
+    	});
+		sensorSocket.write("init 123456677999 " + PRIVATE.token + "\n");	
+	});
+
+	it('broker should send config parameters to sensor if token ok', function (done) {
+		tcpSocketSensorReceiver.on('message', function(message) {
+
+    		console.log("received tcp data: ", message);
+    		var args = message.slice(4);
+    		var argsplit = args.split(" ");
+    		expect(argsplit[0]).to.equal("init");
+    		expect(isNaN(argsplit[1])).to.equal(false);
+    		expect(isNaN(argsplit[2])).to.equal(false);
+    		expect(isNaN(argsplit[3])).to.equal(false);
+    		// check for proper datetime
+    		expect(Date.parse(argsplit[4])).to.be.a("number");
+    		done();
+    	});
+		sensorSocket.write("init 123456677999 " + PRIVATE.token + "\n");
+	});
+
+	it('broker should not register sensor if token not ok', function () {
+		//TBD
+	});
+});
+
--- tests/mocha/database.js
+++ tests/mocha/database.js
@@ -19,84 +19,87 @@ var sensorRemoval = {
     id: undefined
 };
 
-// boot2docker ip
-var host = '192.168.59.103';
+var host;
 
-// boot2dockerIp()
-// .then(function(host){
+describe('Verify correct Database handling', function() {
 
-    describe('Verify correct Database handling', function() {
+    // get host ip
+    before(function(ready){
+        boot2dockerIp()
+        .then(function(h){
+            host = h;
+            ready();
+        })
+        .catch(function(error){
+            console.log("Error determining the host");
+        });     
+    });
 
-        after('Clearing the db', function(ready){
+    after('Clearing the db', function(ready){
+        request.post({
+            url: 'http://' + host + ':4000/removeAllSensors',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }, function(err, result, body){
+            if (!err) {
+                ready();
+                console.log('Sensors cleared');
+            }
+        });
+    });
+
+    describe('Sensors', function () {
+        it("Creating", function (done) {
+            this.timeout(10000);
+
             request.post({
-                url: 'http://' + host + ':4000/removeAllSensors',
+                url: 'http://' + host + ':4000/createSensor',
                 headers: {
                     'Content-Type': 'application/json'
-                }
-            }, function(err, result, body){
-                if (!err) {
-                    ready();
-                    console.log('Sensors cleared');
+                },
+                body: JSON.stringify(sensor)
+            }, function(error, response, body){
+
+                if (error)
+                    console.log('error', error);
+                else{
+                    var created = JSON.parse(body);
+
+                    sensorRemoval.id = created.id;
+
+                    assert.strictEqual('Sensor1', created.name);
+                    assert.strictEqual(290, parseInt(created.sim));
+
+                    done();
                 }
             });
         });
 
-        describe('Sensors', function () {
-            it("Creating", function (done) {
-                this.timeout(10000);
+        it("Removing", function (done) {
+            this.timeout(10000);
 
-                request.post({
-                    url: 'http://' + host + ':4000/createSensor',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(sensor)
-                }, function(error, response, body){
+            request.post({
+                url: 'http://' + host + ':4000/removeSensor',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(sensorRemoval)
+            }, function(error, response, body){
 
-                    if (error)
-                        console.log('error', error);
-                    else{
-                        var created = JSON.parse(body);
+                if (error)
+                    console.log('error', error);
+                else{
+                    var deleted = JSON.parse(body);
 
-                        sensorRemoval.id = created.id;
+                    assert.strictEqual('Sensor1', deleted.name);
+                    assert.strictEqual(290, parseInt(deleted.sim));
 
-                        assert.strictEqual('Sensor1', created.name);
-                        assert.strictEqual(290, parseInt(created.sim));
-
-                        done();
-                    }
-                });
-            });
-
-            it("Removing", function (done) {
-                this.timeout(10000);
-
-                request.post({
-                    url: 'http://' + host + ':4000/removeSensor',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(sensorRemoval)
-                }, function(error, response, body){
-
-                    if (error)
-                        console.log('error', error);
-                    else{
-                        var deleted = JSON.parse(body);
-
-                        assert.strictEqual('Sensor1', deleted.name);
-                        assert.strictEqual(290, parseInt(deleted.sim));
-
-                        done();
-                    }
-                });
+                    done();
+                }
             });
         });
     });
-// })
-// .catch(function(error){
-//     console.log("Error determining the host");
-// });
-
+});
 
 
