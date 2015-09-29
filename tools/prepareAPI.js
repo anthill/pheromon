5ea524be34c2cc4d56d774956d122a499eb60af8
Romain Crestey <rcrestey@gmail.com>
2015-09-29 11:04:35 +0200

    replaced sensor id by sim in prepareAPI.js

--- tools/prepareAPI.js
+++ tools/prepareAPI.js
@@ -12,14 +12,14 @@ module.exports = function(request, origin){
         updateSensor: function(data){
             return request('POST', origin + '/sensor/update', data);
         },
-        deleteSensor: function(id){
-            return request('DELETE', origin + '/sensor/delete/' + id);
+        deleteSensor: function(sim){
+            return request('DELETE', origin + '/sensor/delete/' + sim);
         },
         deleteAllSensors: function(){
             return request('DELETE', origin + '/sensor/deleteAll');
         },
-        getSensor: function(id){
-            return request('GET', origin + '/sensor/get/' + id);
+        getSensor: function(sim){
+            return request('GET', origin + '/sensor/get/' + sim);
         },
         getAllSensors: function(){
             return request('GET', origin + '/sensor/getAll');
