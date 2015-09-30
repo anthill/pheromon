4eba04eee837bf77db7c758a8247fbe4b9c23c74
Romain Crestey <rcrestey@gmail.com>
2015-09-30 12:23:24 +0200

    updated precommit code

---
+++ pre-commit-lint.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var CLIEngine = require('eslint').CLIEngine;
+// 'git ls-files -om --exclude-standard' to list all files that were changed then filter out non-JS files
+
+// process.chdir(__dirname);
+
+var cli = new CLIEngine();
+
+var report = cli.executeOnFiles(['./']);
+
+var formatter = cli.getFormatter();
+
+/*
+ * Wait for the stdout buffer to drain.
+ * See https://github.com/eslint/eslint/issues/317
+ */
+process.on('exit', function() {
+	// output to console
+	console.log('Lint report', formatter(report.results));
+    process.exit();
+});
